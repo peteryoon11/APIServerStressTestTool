c779f8bea6a265a7ac662b6335c2eb6ae37718d9
peteryoon11 <yjisub11@gmail.com>
2019-01-29 22:43:53 +0900

    add code for reading file and print

--- testCode/readFile/main.go
+++ testCode/readFile/main.go
@@ -1,5 +1,27 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+)
+
 func main() {
-	
+	//os.OpenFile("./test.log")
+	f, err := os.OpenFile("./test.log", os.O_RDWR|os.O_CREATE, 0755)
+	if err != nil {
+		log.Fatal(err)
+	}
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		fmt.Println(scanner.Text())
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+	if err := f.Close(); err != nil {
+		log.Fatal(err)
+	}
 }
