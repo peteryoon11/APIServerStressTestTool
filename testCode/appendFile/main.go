4a161c4e2bfdef784e1cf8582d4bdf4afa9bbaa8
peteryoon11 <yjisub11@gmail.com>
2019-02-01 22:15:31 +0900

    add function  reference code

--- testCode/appendFile/main.go
+++ testCode/appendFile/main.go
@@ -25,3 +25,18 @@ func main() {
 		log.Fatal(err)
 	}
 }
+
+func AppendFile() {
+	file, err := os.OpenFile("test.txt", os.O_WRONLY|os.O_APPEND, 0644)
+	if err != nil {
+		log.Fatalf("failed opening file: %s", err)
+	}
+	defer file.Close()
+
+	len, err := file.WriteString(" The Go language was conceived in September 2007 by Robert Griesemer, Rob Pike, and Ken Thompson at Google.")
+	if err != nil {
+		log.Fatalf("failed writing to file: %s", err)
+	}
+	fmt.Printf("\nLength: %d bytes", len)
+	fmt.Printf("\nFile Name: %s", file.Name())
+}
