1e039ffc07d37616624eb6c69a937173974d2ae8
peteryoon11 <yjisub11@gmail.com>
2019-02-10 19:53:07 +0900

    separated folder for struct type

---
+++ pkg/structModule/SimpleBook/structModule.go
@@ -0,0 +1,35 @@
+package structModule
+
+type AuthRequestObject struct {
+	UserId  string `json:"userId"`
+	AuthKey string `json:"AuthKey"`
+}
+
+type ResultObjct struct {
+	Result string `json:"Results"`
+}
+
+type Response struct {
+	Code    int         `json:"code"`
+	Message string      `json:"message"`
+	Data    interface{} `json:"data"`
+}
+
+type ValAPIKey struct {
+	ID     string `json:"ID"`
+	APIKey string `json:"APIKey"`
+}
+type EBookInfo struct {
+	No      int    `json:"No"`
+	Name    string `json:"Name"`
+	ISBN    string `json:"ISBN"`
+	Forsale string `json:"Forsale"`
+	Price   int    `json:"Price"`
+}
+type TestStruct struct {
+	No      int    `json:"No"`
+	Name    string `json:"Name"`
+	ISBN    string `json:"ISBN"`
+	Forsale string `json:"Forsale"`
+	Price   int    `json:"Price"`
+}
